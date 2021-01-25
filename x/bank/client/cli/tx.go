c429d531af601d4fc3fc4ee3cfae9fa8e4a48e8e
osiz-blockchainapp <48212011+osiz-blockchainapp@users.noreply.github.com>
2021-01-25 15:43:12 +0530

    Update tx.go

--- x/bank/client/cli/tx.go
+++ x/bank/client/cli/tx.go
@@ -9,7 +9,7 @@ import (
 	sdk "github.com/osiz-blockchainapp/bitcoiva-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
-	"github.com/cosmos/cosmos-sdk/x/bank/internal/types"
+	"github.com/osiz-blockchainapp/bitcoiva-sdk/x/bank/internal/types"
 )
 
 // GetTxCmd returns the transaction commands for this module
